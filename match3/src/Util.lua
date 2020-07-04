f2b72851d5857a8474542a88225f139bf3dd6659
Macarthur Inbody <133794m3r@gmail.com>
2020-07-04 09:55:00 -0400

    Initial commit for match3 project.

---
+++ match3/src/Util.lua
@@ -0,0 +1,103 @@
+--[[
+    GD50
+    Match-3 Remake
+
+    -- StartState Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Helper functions for writing Match-3.
+]]
+
+--[[
+    Given an "atlas" (a texture with multiple sprites), generate all of the
+    quads for the different tiles therein, divided into tables for each set
+    of tiles, since each color has 6 varieties.
+]]
+function GenerateTileQuads(atlas)
+    local tiles = {}
+
+    local x = 0
+    local y = 0
+
+    local counter = 1
+
+    -- 9 rows of tiles
+    for row = 1, 9 do
+        
+        -- two sets of 6 cols, different tile varietes
+        for i = 1, 2 do
+            tiles[counter] = {}
+            
+            for col = 1, 6 do
+                table.insert(tiles[counter], love.graphics.newQuad(
+                    x, y, 32, 32, atlas:getDimensions()
+                ))
+                x = x + 32
+            end
+
+            counter = counter + 1
+        end
+        y = y + 32
+        x = 0
+    end
+
+    return tiles
+end
+
+--[[
+    Recursive table printing function.
+    https://coronalabs.com/blog/2014/09/02/tutorial-printing-table-contents/
+]]
+function print_r ( t )
+    local print_r_cache={}
+    local function sub_print_r(t,indent)
+        if (print_r_cache[tostring(t)]) then
+            print(indent.."*"..tostring(t))
+        else
+            print_r_cache[tostring(t)]=true
+            if (type(t)=="table") then
+                for pos,val in pairs(t) do
+                    if (type(val)=="table") then
+                        print(indent.."["..pos.."] => "..tostring(t).." {")
+                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+                        print(indent..string.rep(" ",string.len(pos)+6).."}")
+                    elseif (type(val)=="string") then
+                        print(indent.."["..pos..'] => "'..val..'"')
+                    else
+                        print(indent.."["..pos.."] => "..tostring(val))
+                    end
+                end
+            else
+                print(indent..tostring(t))
+            end
+        end
+    end
+    if (type(t)=="table") then
+        print(tostring(t).." {")
+        sub_print_r(t,"  ")
+        print("}")
+    else
+        sub_print_r(t,"  ")
+    end
+    print()
+end
+
+
+---Sets the color used for drawing.
+---@param red number @The amount of red.
+---@param green number @The amount of green.
+---@param blue number @The amount of blue.
+---@param alpha number @The amount of alpha. The alpha value will be applied to all subsequent draw operations, even the drawing of an image.
+---@overload fun(rgba:table):void
+function love.setColor(red,green,blue,alpha)
+    if alpha == nil then
+        alpha = 255
+    end
+    if LOVE_VERSION_11 then
+        love.graphics.setColor(red/255,green/255,blue/255,alpha/255)
+    else
+        love.graphics.setColor(red,green,blue,alpha)
+    end
+end
