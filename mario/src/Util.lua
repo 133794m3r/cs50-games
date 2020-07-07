33453cda7699b136793a514f88f9d7944a79b9ab
Macarthur Inbody <133794m3r@gmail.com>
2020-07-07 19:21:56 -0400

    The first commit of the mario assignment.

---
+++ mario/src/Util.lua
@@ -0,0 +1,161 @@
+--[[
+    GD50
+    Super Mario Bros. Remake
+
+    -- StartState Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Helper functions for writing Match-3.
+]]
+
+--[[
+    Given an "atlas" (a texture with multiple sprites), as well as a
+    width and a height for the tiles therein, split the texture into
+    all of the quads by simply dividing it evenly.
+]]
+function GenerateQuads(atlas, tilewidth, tileheight)
+    local sheetWidth = atlas:getWidth() / tilewidth
+    local sheetHeight = atlas:getHeight() / tileheight
+
+    local sheetCounter = 1
+    local spritesheet = {}
+
+    for y = 0, sheetHeight - 1 do
+        for x = 0, sheetWidth - 1 do
+            spritesheet[sheetCounter] =
+                love.graphics.newQuad(x * tilewidth, y * tileheight, tilewidth,
+                tileheight, atlas:getDimensions())
+            sheetCounter = sheetCounter + 1
+        end
+    end
+
+    return spritesheet
+end
+
+--[[
+    Divides quads we've generated via slicing our tile sheet into separate tile sets.
+]]
+function GenerateTileSets(quads, setsX, setsY, sizeX, sizeY)
+    local tilesets = {}
+    local tableCounter = 0
+    local sheetWidth = setsX * sizeX
+    local sheetHeight = setsY * sizeY
+
+    -- for each tile set on the X and Y
+    for tilesetY = 1, setsY do
+        for tilesetX = 1, setsX do
+            
+            -- tileset table
+            table.insert(tilesets, {})
+            tableCounter = tableCounter + 1
+
+            for y = sizeY * (tilesetY - 1) + 1, sizeY * (tilesetY - 1) + 1 + sizeY do
+                for x = sizeX * (tilesetX - 1) + 1, sizeX * (tilesetX - 1) + 1 + sizeX do
+                    table.insert(tilesets[tableCounter], quads[sheetWidth * (y - 1) + x])
+                end
+            end
+        end
+    end
+
+    return tilesets
+end
+
+--[[
+    Recursive table printing function.
+    https://coronalabs.com/blog/2014/09/02/tutorial-printing-table-contents/
+]]
+function print_r ( t )
+    local print_r_cache={}
+    local function sub_print_r(t,indent)
+        if (print_r_cache[tostring(t)]) then
+            print(indent.."*"..tostring(t))
+        else
+            print_r_cache[tostring(t)]=true
+            if (type(t)=="table") then
+                for pos,val in pairs(t) do
+                    if (type(val)=="table") then
+                        print(indent.."["..pos.."] => "..tostring(t).." {")
+                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+                        print(indent..string.rep(" ",string.len(pos)+6).."}")
+                    elseif (type(val)=="string") then
+                        print(indent.."["..pos..'] => "'..val..'"')
+                    else
+                        print(indent.."["..pos.."] => "..tostring(val))
+                    end
+                end
+            else
+                print(indent..tostring(t))
+            end
+        end
+    end
+    if (type(t)=="table") then
+        print(tostring(t).." {")
+        sub_print_r(t,"  ")
+        print("}")
+    else
+        sub_print_r(t,"  ")
+    end
+    print()
+end
+
+
+
+---Sets the color used for drawing.
+---@param red number @The amount of red.
+---@param green number @The amount of green.
+---@param blue number @The amount of blue.
+---@param alpha number @The amount of alpha. The alpha value will be applied to all subsequent draw operations, even the drawing of an image.
+---@overload fun(rgba:table):void
+function love.setColor(red,green,blue,alpha)
+    if alpha == nil then
+        alpha = 255
+    end
+    if LOVE_VERSION_11 then
+        if type(red) == 'table' then
+            r=red[1]/255
+            g=red[2]/255
+            b=red[3]/255
+            a=red[4]/255
+            love.graphics.setColor(r,g,b,a)
+        else
+            --end
+            love.graphics.setColor(red/255,green/255,blue/255,alpha/255)
+        end
+    else
+        if type(red) == 'table' then
+            print_r(red)
+            love.graphics.setColor(red)
+        else
+            love.graphics.setColor(red,green,blue,alpha)
+
+        end
+    end
+end
+LOVE_VERSION_11 = love.getVersion()
+print(LOVE_VERSION_11)
+printf = function(s,...)
+    return io.write(s:format(...))
+end
+
+function deepcopy(orig, copies)
+    copies = copies or {}
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        if copies[orig] then
+            copy = copies[orig]
+        else
+            copy = {}
+            copies[orig] = copy
+            for orig_key, orig_value in next, orig, nil do
+                copy[deepcopy(orig_key, copies)] = deepcopy(orig_value, copies)
+            end
+            setmetatable(copy, deepcopy(getmetatable(orig), copies))
+        end
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
---
+++ mario/src/states/entity/PlayerJumpState.lua
@@ -0,0 +1,92 @@
+--[[
+    GD50
+    Super Mario Bros. Remake
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+]]
+
+PlayerJumpState = Class{__includes = BaseState}
+
+function PlayerJumpState:init(player, gravity)
+    self.player = player
+    self.gravity = gravity
+    self.cur_time = 0
+    self.frame = 1/60
+    self.animation = Animation {
+        frames = {3},
+        interval = 1
+    }
+    self.player.currentAnimation = self.animation
+end
+
+function PlayerJumpState:enter(params)
+    gSounds['jump']:play()
+    self.player.dy = PLAYER_JUMP_VELOCITY
+end
+
+function PlayerJumpState:update(dt)
+    self.player.currentAnimation:update(dt)
+    self.cur_time = self.cur_time + dt
+    --[[ Due to the fact that Colton never though to think about his gravity systems(again) outside of 60fps and tied
+        it to frame rate. To make it play the same even at higher frame-rates we have to modify the code to either a)
+        Limit the FPS to a maximum of 60. or b) make it so that the gravity system only gets triggered every 1/60th of
+        a second. So that it plays as it did for him.
+    ]]
+    if self.cur_time >= self.frame then
+        self.player.dy = self.player.dy + self.gravity
+        self.cur_time = 0
+    end
+    self.player.y = self.player.y + (self.player.dy * dt)
+
+    -- go into the falling state when y velocity is positive
+    if self.player.dy >= 0 then
+        self.player:changeState('falling')
+    end
+
+    self.player.y = self.player.y + (self.player.dy * dt)
+
+    -- look at two tiles above our head and check for collisions; 3 pixels of leeway for getting through gaps
+    local tileLeft = self.player.map:pointToTile(self.player.x + 3, self.player.y)
+    local tileRight = self.player.map:pointToTile(self.player.x + self.player.width - 3, self.player.y)
+
+    -- if we get a collision up top, go into the falling state immediately
+    if (tileLeft and tileRight) and (tileLeft:collidable() or tileRight:collidable()) then
+        self.player.dy = 0
+        self.player:changeState('falling')
+
+    -- else test our sides for blocks
+    elseif love.keyboard.isDown('left') then
+        self.player.direction = 'left'
+        self.player.x = self.player.x - PLAYER_WALK_SPEED * dt
+        self.player:checkLeftCollisions(dt)
+    elseif love.keyboard.isDown('right') then
+        self.player.direction = 'right'
+        self.player.x = self.player.x + PLAYER_WALK_SPEED * dt
+        self.player:checkRightCollisions(dt)
+    end
+
+    -- check if we've collided with any collidable game objects
+    for k, object in pairs(self.player.level.objects) do
+        if object:collides(self.player) then
+            if object.solid then
+                object.onCollide(object)
+
+                self.player.y = object.y + object.height
+                self.player.dy = 0
+                self.player:changeState('falling')
+            elseif object.consumable then
+                object.onConsume(self.player)
+                table.remove(self.player.level.objects, k)
+            end
+        end
+    end
+
+    -- check if we've collided with any entities and die if so
+    for k, entity in pairs(self.player.level.entities) do
+        if entity:collides(self.player) then
+            gSounds['death']:play()
+            gStateMachine:change('start')
+        end
+    end
+end
